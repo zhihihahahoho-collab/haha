36dac64f3c36a38132fcf70c337eecf8396fb4a0
chisnguyeen <zhihihahahoho@gmail.com>
2026-02-03 03:40:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const API_BASE =
   "https://nonsignificative-marlo-anthroposophic.ngrok-free.dev/api";
 
-const IMAGE_BASE =
-  "https://zhihihahahoho-collab.github.io/haha";
+const IMAGE_BASE = "https://zhihihahahoho-collab.github.io/haha";
+
+
